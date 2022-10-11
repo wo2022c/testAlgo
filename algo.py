0fe630e053d5771ddddc5347abc1da05ee8c7a92
wo2022c <afpawassim@gmail.com>
2022-10-11 16:02:36 +0200

    third

--- algo.py
+++ algo.py
@@ -1,2 +1,18 @@
+import random
+
+
 a=1
 b = 2
+
+print(random.randint(1,99))
+
+taille_tab = 10
+tab = list()
+
+for i in range(taille_tab):
+    print(random.randint(1,99))
+
+tab = list(range(taille_tab)) # list of integers from 1 to 99
+                              # adjust this boundaries to fit your needs
+random.shuffle(tab)
+print(tab)
---
+++ misc.md
@@ -0,0 +1,5 @@
+> citation
+<br>
+https://github.com/luong-komorebi/Markdown-Tutorial/blob/master/README_fr.md
+<br>
+```a = 0```
